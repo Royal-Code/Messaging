71f18ee80504d1ee6c469a6ed94a8b7f01b0b63d
eglauko <eglauko@hotmail.com>
2022-02-15 23:53:30 -0300

    fim do dia

--- Messaging/RoyalCode.RabbitMQ.Components/Connections/ConnectionManager.cs
+++ Messaging/RoyalCode.RabbitMQ.Components/Connections/ConnectionManager.cs
@@ -13,5 +13,6 @@ public class ConnectionManager
         this.connectionPoolFactory = connectionPoolFactory;
     }
 
+
 }
 
--- Messaging/RoyalCode.RabbitMQ.Components/Extensions/RabbitMqComponentsServiceCollectionExtensions.cs
+++ Messaging/RoyalCode.RabbitMQ.Components/Extensions/RabbitMqComponentsServiceCollectionExtensions.cs
@@ -10,7 +10,11 @@ namespace Microsoft.Extensions.DependencyInjection;
 /// </summary>
 public static class RabbitMqComponentsServiceCollectionExtensions
 {
-
+    /// <summary>
+    /// Add services to use RabbitMQ Components.
+    /// </summary>
+    /// <param name="services">The service collection.</param>
+    /// <returns>The same instance of <paramref name="services"/> to chain calls.</returns>
     public static IServiceCollection AddRabbitMQComponents(this IServiceCollection services)
     {
 
